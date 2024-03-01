451c46608add3faaeff47c1d931e6c59ddd01873
esga948 <esga948@vidalibarraquer.net>
2024-03-01 14:27:27 +0100

    c

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -8,10 +8,25 @@ import { InicioAppService } from 'src/app/services/inicio-app.service';
 })
 
 export class NavbarComponent implements OnInit {
+  userAppName: string = '';
   imag: string = '';
 
   constructor(private inicioAppService: InicioAppService) {}
 
   ngOnInit(): void {
+    this.userAppName = this.inicioAppService.getName();
+
+    this.inicioAppService.getU().subscribe((res) => {
+      if (res == '') {
+        this.imag = '../assets/persona.png';
+      } else {
+        this.imag = `http://localhost:8080/${res}`;
+      }
+    });
+  }
+
+  logout(): void {
+    this.inicioAppService.logout();
+    window.location.href = '/login';
   }
 }
