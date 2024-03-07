45837608767800278e093bce969aa0005e0f754c
esga948 <esga948@vidalibarraquer.net>
2024-03-07 15:53:10 +0100

    adaptar altura foto

--- frontend/src/app/pages/register-app/register-app.component.ts
+++ frontend/src/app/pages/register-app/register-app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { InicioAppService } from 'src/app/services/inicio-app.service';
 import { NgForm } from '@angular/forms';
@@ -9,9 +9,18 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './register-app.component.html',
   styleUrls: ['./register-app.component.scss'],
 })
-export class RegisterAppComponent implements OnInit {
+export class RegisterAppComponent implements AfterViewInit, OnInit {
+  @ViewChild('infoContainer') infoContainer!: ElementRef;
+  @ViewChild('myImage') myImage!: ElementRef;
+  
   constructor(private inicioAppService: InicioAppService, private router: Router, private toast: ToastrService) {}
-
+  
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.myImage.nativeElement.style.height = this.infoContainer.nativeElement.offsetHeight + 'px';
+    });
+  }
+  
   ngOnInit(): void {}
 
   onRegister(form: NgForm): void {
